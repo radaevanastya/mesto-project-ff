49295fecd19f0cf4c296cd237aa254ed14749853
radaevanastya <radaevanastya14101996@gmail.com>
2024-09-29 18:10:35 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+const container = document.querySelector('.content');
+const cardList = container.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+const addCard = (el, deleteCard) => {
+  const elementCard = cardTemplate.cloneNode(true);
 
-// @todo: Функция удаления карточки
+  const card = elementCard.querySelector('.card');
+  const cardText = card.querySelector('.card__title');
+  const cardImage = card.querySelector('.card__image');
+  const cardBtn = card.querySelector('.card__delete-button');
 
-// @todo: Вывести карточки на страницу
+  cardText.textContent = el.name;
+  cardImage.alt = el.name;
+  cardImage.src = el.link;
+
+  cardBtn.addEventListener('click', deleteCard);
+
+  return elementCard;
+}
+
+const deleteCard = (evt) => {
+  evt.target.closest('.card').remove();
+}
+
+initialCards.forEach(el => cardList.append(addCard(el, deleteCard)));
